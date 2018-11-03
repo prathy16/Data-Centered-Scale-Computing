029a5f4204bece1a93479018d8fcd9e56da841a6
Prathyusha <31429359+prathy16@users.noreply.github.com>
2018-11-03 10:03:14 -0600

    Delete run.sh

--- PartB/Java/yelp/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# yelp
-
-hadoop fs -mkdir PartB
-hadoop fs -mkdir PartB/Java
-hadoop fs -mkdir PartB/Java/yelp
-
-hadoop jar wordcount.jar wordCount2000.wordCount2000.wordCount2000 s3://wordcount-datasets/ PartB/Java/yelp/output
